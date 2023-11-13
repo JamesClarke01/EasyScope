34583dbf498c94f9f201b0e8096914ba67894a64
James Clarke <jamesclarkedublin@gmail.com>
2023-11-13 23:08:02 +0000

    Manual control from data stream implemented

--- StepperMount/StepperMount.ino
+++ StepperMount/StepperMount.ino
@@ -1,68 +1,73 @@
 #include <Stepper.h>
+#include <Servo.h>
 #include <SoftwareSerial.h>
+
  
+
+//Stepper
+#define STEP_STEP 8
+#define STEP_SPEED 1
 #define STEPS_PER_REV 2048
 
-//Pin Defines
-//Stepper
 #define STEP_IN1 11
 #define STEP_IN2 10
 #define STEP_IN3 9
 #define STEP_IN4 8
+
+//Servo
+#define SERVO_PIN 5
+#define SERVO_STEP 1
+
 
 //HC-05
 #define BT_RX 7
 #define BT_TX 6
 
 Stepper stepper(STEPS_PER_REV, STEP_IN4, STEP_IN2, STEP_IN3, STEP_IN1); 
+Servo servo;
 SoftwareSerial BTSerial(BT_RX, BT_TX); 
  
-int direction = 1;
-int position = 0; //North
+int servoAngle = 80;
+
 
 void setup()
 {
   Serial.begin(9600);
   BTSerial.begin(38400);
-  stepper.setSpeed(10);
+  stepper.setSpeed(STEP_STEP);
+  servo.attach(SERVO_PIN);
+  servo.write(servoAngle);
 }
  
 void loop()
 {  
   if(BTSerial.available()) {
     char input = BTSerial.read();
-    Serial.println(input);
+    //Serial.println(input);
     switch (input) {
-      case 'n':
-        Serial.println("North");
-        
-        stepper.step((0 - position) * 512);
-        position = 0;
+      case 'r':
+        stepper.step(STEP_STEP);
         break;
 
-      case 's':
-        Serial.println("East");
-        stepper.step((1 - position) * 512);
-        position = 1;
+      case 'l':
+        stepper.step(-STEP_STEP);
         break;
-
-      case 'e':
-        Serial.println("South");
-        stepper.step((2 - position) * 512);
-        position = 2;
+      
+      case 'u':
+        if (servoAngle + SERVO_STEP < 180) {
+          servoAngle += SERVO_STEP;
+          servo.write(servoAngle);
+        }
         break;
-
-      case 'w':
-        Serial.println("West");
-        stepper.step((3 - position) * 512);
-        position = 3;
-        break;
-
-      default:
-        Serial.println('Invalid');
-        break;
+      
+      case 'd':
+        if (servoAngle - SERVO_STEP > 80) {
+          servoAngle -= SERVO_STEP;
+          Serial.println(servoAngle);
+          servo.write(servoAngle);
+        }
+        break;      
     }
-
     return 0;
   }
 }
