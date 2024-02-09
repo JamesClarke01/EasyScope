f677c8172051418196d550d4c6b83c3b01d3911d
James <jamesclarkedublin@gmail.com>
2024-02-09 17:08:25 +0000

    Arduino code folder restructure

--- ArduinoMount/Tests/AccelStepperTest/AccelStepperTest.ino
+++ ArduinoMount/Tests/AccelStepperTest/AccelStepperTest.ino
@@ -9,11 +9,12 @@ AccelStepper stepper(AccelStepper::FULL4WIRE, 8,9,10,11);
 void setup()
 {  
   stepper.setMaxSpeed(350);  //Max speed the motor can reliably spin at
-  stepper.moveTo(2048); //Will set a target value of 2048 steps (motor has 2048 steps)     
+  //stepper.moveTo(2048); //Will set a target value of 2048 steps (motor has 2048 steps)     
 }
  
 void loop()
 {  
   stepper.setSpeed(250);  //Must be frequently called
-  stepper.runSpeedToPosition(); //Must be frequently called
+  stepper.runSpeed();
+  //stepper.runSpeedToPosition(); //Must be frequently called
 }
